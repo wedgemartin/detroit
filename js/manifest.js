1a3aaac8db5a52d4f2047524333357b4b670d88a
Wedge Martin <wedge@vivoinspire.com>
2016-04-19 09:15:15 -0700

    duh

--- js/manifest.js
+++ js/manifest.js
@@ -2,7 +2,7 @@ load("js/routes.js")
 load("js/api.js")
 load("js/models/base.js");
 
-var mc = new com.mongodb.Client("localhost", 27017);
+var mc = new com.mongodb.MongoClient("localhost", 27017);
 var mongo = mc.getDatabase("detroit");
 API.setMongo(mongo);
 
